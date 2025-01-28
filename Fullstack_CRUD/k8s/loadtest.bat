266deccb0964a9dfd86981c0c04da950d3637043
DNP DEVELOPER <44169383+dnp176@users.noreply.github.com>
2025-01-28 18:11:07 +0530

    Update file after load balancing testing

--- Fullstack_CRUD/k8s/loadtest.bat
+++ Fullstack_CRUD/k8s/loadtest.bat
@@ -2,14 +2,14 @@
 setlocal enabledelayedexpansion
 
 :: URL ko set karen
-set URL=http://localhost:7001/api/v1/employees
+set URL=http://backend.local:8081/api/v1/employees
 
 :: Total requests kitni bhejni hain
 set requests=1000
 
 :: Loop se requests bhejne ka kaam
 for /L %%i in (1,1,%requests%) do (
-    echo Request %%i bheja ja raha hai...
+    echo Request %%i Successfully Request Send...
     curl -s -o nul %URL%
 )
 
